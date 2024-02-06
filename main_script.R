f6d975e2e67b19fe497516bae13c13e20b87e621
anaisjolivet <jolivetbanais@gmail.com>
2024-02-06 11:57:42 +0100

    going back

--- main_script.R
+++ main_script.R
@@ -1,4 +1,10 @@
 library(dplyr)
 library(ggplot2)
+ibrary(readr)
 
-iris <- data(iris)
+# reading data
+iris_data <- read_csv('raw_data/iris.csv')
+
+# subsetting data
+setosa_subset <- iris_data |>
+  filter(Species == "setosa")
